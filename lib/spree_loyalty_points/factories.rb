a259d56e8070f181397622a934edd2b41b3d3ec7
jatin-baweja <jatin.baweja@vinsol.com>
2014-01-29 15:00:01 +0530

    Added factories for specs

--- lib/spree_loyalty_points/factories.rb
+++ lib/spree_loyalty_points/factories.rb
@@ -3,4 +3,69 @@ FactoryGirl.define do
   #
   # Example adding this to your spec_helper will load these Factories for use:
   # require 'spree_loyalty_points/factories'
+
+  factory :loyalty_points_transaction, :class => 'Spree::LoyaltyPointsTransaction' do
+    loyalty_points { (10..99).to_a.sample }
+    updated_balance { (100..999).to_a.sample }
+    comment { Faker::Lorem.words(3).join(' ') }
+    transaction_type "Credit"
+
+    association :user, factory: :user_with_loyalty_points
+
+    factory :loyalty_points_debit_transaction do
+      transaction_type "Debit"
+    end
+
+  end
+
+  factory :user_with_loyalty_points, parent: :user do
+    loyalty_points_balance { (100..999).to_a.sample }
+
+    ignore do
+      transactions_count 5
+    end
+
+    after(:create) do |user, evaluator|
+      create_list(:loyalty_points_transaction, evaluator.transactions_count, user: user)
+    end
+  end
+
+  factory :order_with_loyalty_points, parent: :order do
+
+    association :user, factory: :user_with_loyalty_points
+
+    ignore do
+      transactions_count 5
+    end
+
+    after(:create) do |order, evaluator|
+      create_list(:loyalty_points_transaction, evaluator.transactions_count, source: order)
+    end
+
+    factory :shipped_order_with_loyalty_points do
+      ignore do
+        shipments_count 5
+      end
+
+      after(:create) do |order, evaluator|
+        create_list(:shipment, evaluator.shipments_count, order: order, state: "shipped")
+      end
+    end
+
+  end
+
+  factory :payment_with_loyalty_points, parent: :payment do
+
+    association :order, factory: :order_with_loyalty_points
+
+  end
+
+  factory :return_authorization_with_loyalty_points, parent: :return_authorization do
+    loyalty_points { (50..99).to_a.sample }
+    loyalty_points_transaction_type "Debit"
+
+    association :order, factory: :shipped_order_with_loyalty_points
+
+  end
+
 end

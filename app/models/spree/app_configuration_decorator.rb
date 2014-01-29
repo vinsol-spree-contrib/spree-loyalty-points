b4b4f5fbb15c95d772969dba8f55083a7b1b05f6
jatin-baweja <jatin.baweja@vinsol.com>
2014-01-29 14:44:09 +0530

    Changed preference defaults

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,7 +1,7 @@
 Spree::AppConfiguration.class_eval do
-  preference :loyalty_points_unit_amount, :decimal
-  preference :loyalty_points_awarding_unit, :integer, :default => 0
-  preference :loyalty_points_redeeming_balance, :decimal
-  preference :loyalty_points_conversion_rate, :decimal, :default => 0.0
+  preference :min_amount_required_to_get_loyalty_points, :decimal, :default => 20.0
+  preference :loyalty_points_awarding_unit, :decimal, :default => 0.0
+  preference :loyalty_points_redeeming_balance, :integer, :default => 50
+  preference :loyalty_points_conversion_rate, :decimal, :default => 5.0
   preference :loyalty_points_award_period, :integer, :default => 1
 end

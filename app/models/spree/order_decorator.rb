864e3a4eb2c102ed9f2b9b327b511378ef34915a
jatin-baweja <jatin.baweja@vinsol.com>
2014-01-29 14:46:55 +0530

    Changed create_loyalty_points_transactions to new_loyalty_points_transactions

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,33 +4,35 @@ Spree::Order.class_eval do
   def add_loyalty_points
     loyalty_points_earned = loyalty_points_for(item_total)
     if !payment_by_loyalty_points?
-      create_loyalty_points_transaction(loyalty_points_earned, 'Credit')
+      new_loyalty_points_transaction(loyalty_points_earned, 'Credit')
     end
   end
 
   def redeem_loyalty_points
     loyalty_points_redeemed = loyalty_points_for(total, 'redeem')
-    if payment_by_loyalty_points?
-      create_loyalty_points_transaction(loyalty_points_redeemed, 'Debit')
+    if payment_by_loyalty_points? && redeemable_loyalty_points_balance?(total)
+      new_loyalty_points_transaction(loyalty_points_redeemed, 'Debit')
     end
   end
 
   def update_loyalty_points(quantity, trans_type)
     loyalty_points_debit_quantity = [user.loyalty_points_balance, loyalty_points_for(total), quantity].min
-    create_loyalty_points_transaction(loyalty_points_debit_quantity, trans_type)
+    new_loyalty_points_transaction(loyalty_points_debit_quantity, trans_type)
   end
 
-  def create_loyalty_points_transaction(quantity, trans_type)
+  def new_loyalty_points_transaction(quantity, trans_type)
     if quantity != 0
       user.loyalty_points_transactions.create(source: self, loyalty_points: quantity, transaction_type: trans_type)
     end
   end
 
   def loyalty_points_for(amount, purpose = 'award')
-    loyalty_points = if purpose == 'award'
-      (amount / Spree::Config.loyalty_points_unit_amount).floor * Spree::Config.loyalty_points_awarding_unit
+    loyalty_points = if purpose == 'award' && eligible_for_loyalty_points?(amount)
+      (amount * Spree::Config.loyalty_points_awarding_unit).floor
     elsif purpose == 'redeem'
       (amount / Spree::Config.loyalty_points_conversion_rate).ceil
+    else
+      0
     end
   end
 
@@ -38,8 +40,12 @@ Spree::Order.class_eval do
     payments.includes(:payment_method).any? { |payment| payment.payment_method.type == "Spree::PaymentMethod::LoyaltyPoints" && payment.state != "invalid" }
   end
 
-  def eligible_for_loyalty_points?
-    item_total >= Spree::Config.loyalty_points_unit_amount
+  def eligible_for_loyalty_points?(amount)
+    amount >= Spree::Config.min_amount_required_to_get_loyalty_points
+  end
+
+  def redeemable_loyalty_points_balance?(amount)
+    amount >= Spree::Config.loyalty_points_redeeming_balance
   end
 
   def mark_awarded

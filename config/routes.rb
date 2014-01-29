d9cd276a0461ccfe29f15e6a2d669165695e84d8
jatin-baweja <jatin.baweja@vinsol.com>
2014-01-29 14:58:21 +0530

    Added routes for order transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+
   resources :loyalty_points, only: [:index]
 
   namespace :admin do
     resources :users do
-      resources :loyalty_points, only: [:index, :new, :create]
+      resources :loyalty_points, only: [:index, :new, :create] do
+        get 'order_transactions/:order_id', action: :order_transactions, on: :collection
+      end
     end
   end
 

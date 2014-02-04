c512665b2e24996c7cebe9167f37cf85b96bc255
jatin-baweja <jatin.baweja@vinsol.com>
2014-02-04 20:09:36 +0530

    Subclassing admin::resource controller

--- app/controllers/spree/admin/loyalty_points_transactions_controller.rb
+++ app/controllers/spree/admin/loyalty_points_transactions_controller.rb
@@ -1,26 +1,9 @@
 #TODO -> We can use resource_controller of spree here.
 
-class Spree::Admin::LoyaltyPointsController < Spree::Admin::BaseController
-  before_action :set_user
-
-  def index
-    @loyalty_points = @user.loyalty_points_transactions.includes(:source).order(updated_at: :desc).
-      page(params[:page]).
-      per(params[:per_page] || Spree::Config[:orders_per_page])
-  end
-
-  def new
-    @loyalty_points_transaction = @user.loyalty_points_transactions.build
-  end
-
-  def create
-    @loyalty_points_transaction = @user.loyalty_points_transactions.create(loyalty_points_transaction_params)
-    if @loyalty_points_transaction.persisted?
-      redirect_to admin_users_path, notice: "Successfully updated user's Loyalty Points"
-    else
-      render action: :new
-    end
-  end
+class Spree::Admin::LoyaltyPointsTransactionsController < Spree::Admin::ResourceController
+  before_action :set_user, only: [:order_transactions]
+  belongs_to 'spree/user'
+  before_action :set_ordered_transactions, only: [:index]
 
   def order_transactions
     order = Spree::Order.find_by(id: params[:order_id])
@@ -40,7 +23,7 @@ class Spree::Admin::LoyaltyPointsController < Spree::Admin::BaseController
     end
   end
 
-  private
+  protected
 
     def set_user
       unless @user = Spree::User.find_by(id: params[:user_id])
@@ -50,6 +33,28 @@ class Spree::Admin::LoyaltyPointsController < Spree::Admin::BaseController
 
     def loyalty_points_transaction_params
       params.require(:loyalty_points_transaction).permit(:loyalty_points, :type, :comment, :source_id, :source_type)
+    end
+
+    def build_resource
+      if params[:loyalty_points_transaction].present? && params[:loyalty_points_transaction][:type].present?
+        parent.send(association_name(params[:loyalty_points_transaction][:type])).build
+      else
+        parent.send(controller_name).build
+      end
+    end
+
+    def association_name(klass)
+      klass.gsub('Spree::', '').pluralize.underscore
+    end
+
+    def collection_url
+      admin_user_loyalty_points_url(parent)
+    end
+
+    def set_ordered_transactions
+      @loyalty_points_transactions = @loyalty_points_transactions.order(updated_at: :desc).
+        page(params[:page]).
+        per(params[:per_page] || Spree::Config[:orders_per_page])
     end
 
 end

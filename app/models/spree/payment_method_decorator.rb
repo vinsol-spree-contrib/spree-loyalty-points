2bf9d519d94c0fa05bd9b3f48a2fb137de660a85
jatin-baweja <jatin.baweja@vinsol.com>
2014-03-04 15:59:33 +0530

    Added scope for Loyalty Points Payment Method

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,7 +1,9 @@
 Spree::PaymentMethod.class_eval do
 
+  scope :loyalty_points_type, -> { where(type: 'Spree::PaymentMethod::LoyaltyPoints') }
+
   def self.loyalty_points_id_included?(method_ids)
-    where(type: 'Spree::PaymentMethod::LoyaltyPoints').where(id: method_ids).size != 0
+    loyalty_points_type.where(id: method_ids).size != 0
   end
 
 end

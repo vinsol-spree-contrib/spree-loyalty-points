723f4d8425eb59c9cf0e2f86ac121d54d79c0e08
jatin-baweja <jatin.baweja@vinsol.com>
2014-03-26 19:32:36 +0530

    Added a check if lock_version column already exists in spree_users

--- db/migrate/20140207055836_add_lock_version_to_spree_user.rb
+++ db/migrate/20140207055836_add_lock_version_to_spree_user.rb
@@ -1,5 +1,5 @@
 class AddLockVersionToSpreeUser < ActiveRecord::Migration
   def change
-    add_column :spree_users, :lock_version, :integer, default: 0, null: false
+    add_column :spree_users, :lock_version, :integer, default: 0, null: false unless column_exists?(:spree_users, :lock_version)
   end
 end

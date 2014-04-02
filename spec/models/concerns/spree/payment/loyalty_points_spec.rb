1eb92721655f7337461e431d9b6b899b44b6bad0
jatin-baweja <jatin.baweja@vinsol.com>
2014-04-02 19:30:21 +0530

    Removed TODO comments

--- spec/models/concerns/spree/payment/loyalty_points_spec.rb
+++ spec/models/concerns/spree/payment/loyalty_points_spec.rb
@@ -181,8 +181,6 @@ shared_examples_for "Payment::LoyaltyPoints" do
         resource_instance.order.user.stub(:loyalty_points_balance).and_return(20)
       end
 
-      #[TODO] -> Change this test case description as "it is adding error"
-      
       it "should add error to loyalty_points_balance" do
         min_balance = Spree::Config.loyalty_points_redeeming_balance
         resource_instance.send(:redeemable_user_balance)

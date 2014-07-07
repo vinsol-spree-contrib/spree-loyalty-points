f67dfe99c78dc8c75f4e526b0ec3a0e6f83d7d6d
mohit <mohit.bansal@vinsol.com>
2014-07-07 18:13:58 +0530

    fix sufficicent_loyalty_points method to find payment_method_ids when payments_attributes is a hash

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -5,7 +5,17 @@ module Spree
     private
 
       def sufficient_loyalty_points
-        payment_method_ids = params[:order][:payments_attributes].collect do |payment|
+        payment_method_ids = params[:order][:payments_attributes].collect do |v1, v2|
+          #params[:order][:payments_attributes] can be a hash or array. E.g. 
+          # params[:order][:payments_attributes] = { 
+          #   “0” => {“payment_method_id” => 8, “amount” => 100}, 
+          #   “1” => {“payment_method_id” => 9, “amount” => 50} 
+          # }
+          # params[:order][:payments_attributes] = [
+          #   {“payment_method_id” => 8, “amount” => 100},
+          #   {“payment_method_id” => 9, “amount” => 50}
+          # ]
+          payment = v2 || v1
           payment["payment_method_id"]
         end
         if Spree::PaymentMethod.loyalty_points_id_included?(payment_method_ids) && !@order.user.has_sufficient_loyalty_points?(@order)

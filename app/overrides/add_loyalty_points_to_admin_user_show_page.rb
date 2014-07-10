fdf91e8decdbec3e66f86e04d19632a89ac41486
mohit <mohit.bansal@vinsol.com>
2014-07-10 14:37:24 +0530

    move loyalty points details from users show to edit page

--- app/overrides/add_loyalty_points_to_admin_user_show_page.rb
+++ app/overrides/add_loyalty_points_to_admin_user_show_page.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(:virtual_path => 'spree/admin/users/show',
+Deface::Override.new(:virtual_path => 'spree/admin/users/edit',
   :name => 'add_loyalty_points_to_admin_user_show_page',
-  :insert_after => "tr[data-hook='roles']",
+  :insert_after => "#admin_user_edit_api_key",
   :text => "
   <tr data-hook='loyalty-points'>
     <th><%= Spree.t(:loyalty_points_balance) %></th>

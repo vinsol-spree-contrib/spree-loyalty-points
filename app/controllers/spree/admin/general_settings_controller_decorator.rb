fb74bd8d1664d3d925b233f69c24975b06fc6498
GarPit <garik.piton@gmail.com>
2016-11-07 13:05:45 +0300

    russian localization + put some strings into locale files

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++ app/controllers/spree/admin/general_settings_controller_decorator.rb
@@ -7,9 +7,9 @@ Spree::Admin::GeneralSettingsController.class_eval do
     def set_loyalty_points_settings
       @preferences_loyalty_points = {
         min_amount_required_to_get_loyalty_points: [""],
-        loyalty_points_awarding_unit: ["For example: Set this as 10 if we wish to award 10 points for $1 spent on the site."],
+        loyalty_points_awarding_unit: [Spree.t(:loyalty_points_awarding_unit_hint)],
         loyalty_points_redeeming_balance: [""],
-        loyalty_points_conversion_rate: ["For example: Set this value to 5 if we wish 1 loyalty point is equivalent to $5"],
+        loyalty_points_conversion_rate: [Spree.t(:loyalty_points_conversion_rate_hint)],
         loyalty_points_award_period: [""]
       }
     end

c70f8f72c29e1c8b035ea3f064bdf7d6517b704c
Harsh Agrawal <harsh@vinsol.com>
2018-01-30 11:37:38 +0530

    Fix issues arising due to absence of spree_current_user

--- app/controllers/spree/loyalty_points_controller.rb
+++ app/controllers/spree/loyalty_points_controller.rb
@@ -2,9 +2,13 @@ module Spree
   class LoyaltyPointsController < StoreController
 
     def index
-      @loyalty_points_transactions = spree_current_user.loyalty_points_transactions.includes(:source).order(updated_at: :desc).
-                                       page(params[:page]).
-                                       per(params[:per_page] || Spree::Config[:orders_per_page])
+      if spree_current_user.present?
+        @loyalty_points_transactions = spree_current_user.loyalty_points_transactions.includes(:source).order(updated_at: :desc).
+                                        page(params[:page]).
+                                        per(params[:per_page] || Spree::Config[:orders_per_page])
+      else
+        @loyalty_points_transactions = []
+      end
     end
 
   end
